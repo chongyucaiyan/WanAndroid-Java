8c60799f54485e99578f56f9c5fd85e4a7fc2b1b
chongyucaiyan <chongyucaiyan@163.com>
2020-04-24 23:55:20 +0800

    fix(main activity): remove enter animation

--- app/src/main/java/com/github/cyc/wanandroid/module/main/activity/MainActivity.java
+++ app/src/main/java/com/github/cyc/wanandroid/module/main/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.github.cyc.wanandroid.module.main.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -47,6 +49,12 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
     public static void start(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
         context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
     }
 
     @Override
